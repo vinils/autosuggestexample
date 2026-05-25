d2942d05d15a6e45c014cc2f2709450d6eb4a18e
VLS <vini_ls@hotmail.com>
2026-05-25 13:27:49 -0300

    tryfixndebug

--- index.js
+++ index.js
@@ -4,10 +4,8 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import appJson from './app.json';
 
-const appName = appJson.name;
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent("example", () => App);
 
 if (typeof document !== 'undefined') {
   const rootTag = document.getElementById('root') || document.createElement('div');
@@ -16,7 +14,7 @@ if (typeof document !== 'undefined') {
     document.body.appendChild(rootTag);
   }
 
-  AppRegistry.runApplication(appName, {
+  AppRegistry.runApplication("example", {
     rootTag,
   });
 }
